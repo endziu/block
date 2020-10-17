e620e95f8aabbfc95c2d7b2785eda9b8a475cbd0
endziu <koper.andrzej@gmail.com>
2020-10-18 00:00:41 +0200

    refactor small functions to utils.js

--- index.js
+++ index.js
@@ -2,33 +2,37 @@ const fetch = require('node-fetch')
 const express = require('express')
 const server = express()
 
-const { __, map, reduce, filter, pipe, prop, juxt, head, median, mean, min, max, divide, sort, reverse } = require('ramda')
+const { 
+  map,
+  filter,
+  pipe,
+  prop,
+  median,
+  mean,
+  sort,
+  reverse
+} = require('ramda')
+
+const { 
+  WeiToGwei,
+  GweiToEther,
+  WeiToEther,
+  wrap,
+  fetchJson,
+  blockNumUrl,
+  blockUrl,
+  inRange
+} = require('./utils.js')
 
 const { ETHERSCAN_APIKEY } = require('./config.js')
-
-const WeiToGwei = divide(__, 1e9)
-const GweiToEther = WeiToGwei
-const WeiToEther = pipe(WeiToGwei, GweiToEther)
-const wrap = fn => (...args) => fn(...args).catch(args[2])
-
-const getBlock = wrap(async () => {
-  const eth_block_num = `https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=${ETHERSCAN_APIKEY}`
-  const blockNumRes = await fetch(eth_block_num)
-  const blockNumBody = await blockNumRes.json()
-  const blockNum = blockNumBody.result
-
-  const eth_block = `https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${blockNum}&boolean=true&apikey=${ETHERSCAN_APIKEY}`
-  const blockRes = await fetch(eth_block)
-  const blockBody = await blockRes.json()
-  const block = blockBody.result
-  return [block, blockNum]
-})
 
 let data = []
 
 const main = async () => {
-  const [block, blockNumber] = await getBlock()
+  const blockNumber = await fetchJson(blockNumUrl(ETHERSCAN_APIKEY))
+  const block = await fetchJson(blockUrl(blockNumber, ETHERSCAN_APIKEY))
   const { transactions } = block
+
   const gwei = map( pipe(prop('gasPrice'), WeiToGwei), transactions )
   const sorted = sort((a,b) => a - b, gwei)
   
@@ -38,15 +42,15 @@ const main = async () => {
   const blockMax = sorted[sorted.length - 1]
 
   const buckets = {
-    "0-20": filter((n) => n > 0 && n < 20, sorted).length,
-    "20-40": filter((n) => n > 20 && n < 40, sorted).length,
-    "40-60": filter((n) => n > 40 && n < 60, sorted).length,
-    "60-80": filter((n) => n > 60 && n < 80, sorted).length,
-    "80-100": filter((n) => n > 80 && n < 100, sorted).length,
-    "100-#": filter((n) => n > 100, sorted).length
+    "0-20": filter(inRange(0, 20), sorted).length,
+    "20-40": filter(inRange(20, 40), sorted).length,
+    "40-60": filter(inRange(40, 60), sorted).length,
+    "60-80": filter(inRange(60, 80), sorted).length,
+    "80-100": filter(inRange(80, 100), sorted).length,
+    "100-#":  filter(inRange(100), sorted).length
   }
 
-  if (data.length > 239) {
+  if (data.length > 127) {
     data = data.slice(1)
   }
 
@@ -55,7 +59,14 @@ const main = async () => {
     return
   }
 
-  data.push({ blockNumber:[blockNumber, Number(blockNumber)], min: blockMin, mean: blockMean, median: blockMedian, max: blockMax, buckets })
+  data.push({
+    blockNumber: [blockNumber, Number(blockNumber)],
+    min: blockMin,
+    mean: blockMean,
+    median: blockMedian,
+    max: blockMax,
+    buckets
+  })
 
   console.clear()
   console.log(data && data[data.length -1])
---
+++ utils.js
@@ -0,0 +1,33 @@
+const { __, pipe, divide } = require('ramda')
+const fetch = require('node-fetch')
+
+const WeiToGwei = divide(__, 1e9)
+const GweiToEther = WeiToGwei
+const WeiToEther = pipe(WeiToGwei, GweiToEther)
+const wrap = fn => (...args) => fn(...args).catch(args[2])
+
+const fetchJson = async (url) => {
+  const response = await fetch(url)
+  const json = await response.json()
+  const { result } = json
+  return result
+}
+
+const blockNumUrl = (key) =>
+  `https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey=${key}`
+
+const blockUrl = (num, key) =>
+  `https://api.etherscan.io/api?module=proxy&action=eth_getBlockByNumber&tag=${num}&boolean=true&apikey=${key}`
+
+const inRange = (n1, n2 = Infinity) => (p) => p > n1 && p < n2
+
+module.exports = {
+  WeiToGwei,
+  GweiToEther,
+  WeiToEther,
+  wrap,
+  fetchJson,
+  blockNumUrl,
+  blockUrl,
+  inRange
+}
